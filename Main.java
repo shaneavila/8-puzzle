ea55c22075a96252da1d22ac23c100349ee3af8c
Shane <avila.shane@yahoo.com>
2018-08-30 19:11:23 -0700

    Move work to new branch

--- Main.java
+++ Main.java
@@ -2,9 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter a size for the sliding block puzzle: ");
-        int boardSize = input.nextInt();
-        input.nextLine();
+
     }
 }
